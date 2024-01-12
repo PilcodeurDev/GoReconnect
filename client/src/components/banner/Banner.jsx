e5b797f648ff0fc3f6582f38a1d37c96a0d59fb7
simon despres <simondprs62790@hotmail.fr>
2024-01-12 11:39:30 +0100

    setup banner

---
+++ client/src/components/banner/Banner.jsx
@@ -0,0 +1,11 @@
+import "./Banner.css";
+
+export default function Banner() {
+  return (
+    <section>
+      <div className="bannerImg">
+        <h1>Kinésiologie Animanière 25</h1>
+      </div>
+    </section>
+  );
+}

cba409a7c76ead84dac9fa4d9c1a4378b6fe228c
simon despres <simondprs62790@hotmail.fr>
2024-01-13 14:35:17 +0100

    test use html balise img for debug

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -2,12 +2,14 @@
  * The internal imports
  */
 import "./Banner.css";
+import AlixAndShiba from "../../../public/images/alixAndShiba.jpg";
 
 export default function Banner() {
   return (
     <section>
       <div className="container">
-        <div className="bannerImg">
+        <div className="banner">
+          <img src={AlixAndShiba} alt="alix and shiba" />
           <h1>Kinésiologie Animanière 25</h1>
         </div>
       </div>

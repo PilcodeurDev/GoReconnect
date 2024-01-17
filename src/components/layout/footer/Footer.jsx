66cf54374ff9b3e75492da759fe7bb7772e7bae8
simon despres <simondprs62790@hotmail.fr>
2024-01-17 09:38:22 +0100

    setup footer and css

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -2,11 +2,46 @@
  * The internal imports
  */
 import "./Footer.css";
+import { Link } from "react-router-dom";
+import { fbk, insta, logo, whatsApp } from "../../../assets";
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <h2>i'm footer</h2>
+    <footer>
+        <div>
+          <img
+            src={logo}
+            alt="logo kinésiologie animalière 25"
+            className="logoKa25"
+          />
+        </div>
+        <div>
+          <div>
+            <span>
+              © 2024 Kinésiologie Animalière 25 Tous droits réservés .
+            </span>
+            <span>
+              <Link to="#" className="lien">Conditions Générales de Vente & Utilisation</Link>
+            </span>
+          </div>
+          <p>
+            <span>
+              <Link to="#" className="lien">Mentions légales</Link>
+            </span>
+            . Design & Développement par PilcodeurDev
+          </p>
+        </div>
+        <div className="logos">
+          <a href="https://www.facebook.com/kinesiologieanimaliere25">
+            <img src={fbk} alt="logo facebook" />
+          </a>
+          <a href="https://www.instagram.com/kinesiologie_animaliere_25/">
+            <img src={insta} alt="logo instagram" />
+          </a>
+          <a href="https://www.instagram.com/kinesiologie_animaliere_25/">
+            <img src={whatsApp} alt="logo instagram" />
+          </a>
+        </div>
     </footer>
   );
 }

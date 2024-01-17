78932a396b1730d982c535da87b95350842ea387
simon despres <simondprs62790@hotmail.fr>
2024-01-17 10:52:56 +0100

    add lien for navigate when the user is on the footer

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -20,40 +20,69 @@ export default function Footer() {
 
   return (
     <footer>
-        <Link to="/#moveUp">
-          <img
-            src={logo}
-            alt="logo kinésiologie animalière 25"
-            className="logoKa25"
-          />
-        </Link>
-        <div>
+      <Link to="/#moveUp">
+        <img
+          src={logo}
+          alt="logo kinésiologie animalière 25"
+          className="logoKa25"
+        />
+      </Link>
+      <div className="textCenter">
+        <div className="lienNavigation">
           <div>
+            <Link to="/#moveUp" className="lien">
+              Accueil
+            </Link>
+          </div>
+          <div>
+            <Link to="/#laKinesiologie" className="lien">
+              Mes prestations
+            </Link>
+          </div>
+          <div>
+            <Link to="/#quiJeSuis" className="lien">
+              Qui suis-je ?
+            </Link>
+          </div>
+          <div>
+            <Link to="/#reservation" className="lien">
+              Rendez-vous
+            </Link>
+          </div>
+        </div>
+        <div className="lienCenter">
+          <div>
+            <span>© 2024 Kinésiologie Animalière 25 Tous droits réservés.</span>
             <span>
-              © 2024 Kinésiologie Animalière 25 Tous droits réservés .
+              <Link to="#" className="lien">
+                Mentions légales
+              </Link>
             </span>
             <span>
-              <Link to="#" className="lien">Conditions Générales de Vente & Utilisation</Link>
+              <Link to="/condition-general-de-vente" className="lien">
+                CGV
+              </Link>
+            </span>
+            <span>
+              <Link to="/condition-general-dutilisation" className="lien">
+                CGU
+              </Link>
             </span>
           </div>
-          <p>
-            <span>
-              <Link to="#" className="lien">Mentions légales</Link>
-            </span>
-            . Design & Développement par {PilcodeurDev}
-          </p>
+          <div>Design & Développement par {PilcodeurDev}</div>
         </div>
-        <div className="logos">
-          <a href="https://www.facebook.com/kinesiologieanimaliere25">
-            <img src={fbk} alt="logo facebook" />
-          </a>
-          <a href="https://www.instagram.com/kinesiologie_animaliere_25/">
-            <img src={insta} alt="logo instagram" />
-          </a>
-          <a href="https://www.instagram.com/kinesiologie_animaliere_25/">
-            <img src={whatsApp} alt="logo instagram" />
-          </a>
-        </div>
+      </div>
+      <div className="logos">
+        <a href="https://www.facebook.com/kinesiologieanimaliere25">
+          <img src={fbk} alt="logo facebook" />
+        </a>
+        <a href="https://www.instagram.com/kinesiologie_animaliere_25/">
+          <img src={insta} alt="logo instagram" />
+        </a>
+        <a href="https://www.instagram.com/kinesiologie_animaliere_25/">
+          <img src={whatsApp} alt="logo instagram" />
+        </a>
+      </div>
     </footer>
   );
 }

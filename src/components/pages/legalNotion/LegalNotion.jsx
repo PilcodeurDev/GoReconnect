0a1488928a6abdc6ef3cd82a7020473227b14bcc
simon despres <simondprs62790@hotmail.fr>
2024-01-17 12:36:54 +0100

    clear the code

--- src/components/pages/legalNotion/LegalNotion.jsx
+++ src/components/pages/legalNotion/LegalNotion.jsx
@@ -4,14 +4,16 @@
 import "./LegalNotion.css";
 
 export default function LegalNotion() {
-  const nomDuSite = "Kinesiologie Animaliere 25";
-  const urlDuSite = "https://www.kinesiologie-animaliere-25.fr";
-  const proprietaire = "Alix Lucidarme";
-  const adresseProprietaire = "32 Bis rue de l'Etang, 25560 Frasne";
-  const telephoneProprietaire = "+33668352377";
-  const hebergeur = "Wix Online Platform Limited";
-  const adresseHebergeur = "1 Grant’s Row, Dublin 2 D02HX96, Ireland";
-  const telephoneHebergeur = "(+1) 415 358 0857";
+  const data = [
+    { site: "Kinesiologie Animaliere 25" },
+    { url: "https://www.kinesiologie-animaliere-25.fr" },
+    { owner: "Alix Lucidarme" },
+    { adressOwner: "32 Bis rue de l'Etang, 25560 Frasne" },
+    { phoneOwner: "+33668352377" },
+    { host: "Wix Online Platform Limited" },
+    { adressHost: "1 Grant’s Row, Dublin 2 D02HX96, Ireland" },
+    { phoneHost: "(+1) 415 358 0857" },
+  ];
   return (
     <section>
       <div className="container notionsLegales">
@@ -21,27 +23,26 @@ export default function LegalNotion() {
         <p>
           Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004
           pour la confiance en l'économie numérique, il est précisé aux
-          utilisateurs du site {nomDuSite} l'identité des différents
+          utilisateurs du site {data[0].site} l'identité des différents
           intervenants dans le cadre de sa réalisation et de son suivi.
         </p>
         <h3>Edition du site</h3>
         <p>
-          Le présent site, accessible à l’URL {urlDuSite}
-          (le « Site »), est édité par : {proprietaire}, résidant au{" "}
-          {adresseProprietaire}, de nationalité Française (France), né(e) le
+          Le présent site, accessible à l’URL {data[1].url}
+          (le « Site »), est édité par : {data[2].owner}, résidant au{" "}
+          {data[3].adressOwner}, de nationalité Française (France), né(e) le
           25/09/1992,
         </p>
         <h3>Hébergement</h3>
         <p>
-          Le Site est hébergé par la société {hebergeur}, situé
-          {adresseHebergeur}, (contact téléphonique ou email :{" "}
-          {telephoneHebergeur}).
+          Le Site est hébergé par la société {data[5].host}, situé{" "}
+          {data[6].adressHost}, (contact téléphonique ou email : {data[7].phoneHost}).
         </p>
         <h3>Directeur de publication</h3>
-        Le Directeur de la publication du Site est {proprietaire}.
+        Le Directeur de la publication du Site est {data[2].owner}.
         <h3>Nous contacter</h3>
-        <p>Par téléphone : {telephoneProprietaire}</p>
-        <p>Par courrier : {adresseProprietaire}</p>
+        <p>Par téléphone : {data[4].phoneOwner}</p>
+        <p>Par courrier : {data[3].adressOwner}</p>
         <i>Génération des mentions légales par Legalstart.</i>
       </div>
     </section>

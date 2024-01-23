443acb60fb2cfc173d04a0bf7888a8da2b6dd207
simon despres <simondprs62790@hotmail.fr>
2024-01-23 10:41:10 +0100

    delete AOS init and change name in banner

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,5 +5,7 @@ import insta from "./InstagramLogo.png";
 import whatsApp from "./WhatsAppLogo.png";
 import alixAndShiba from "./alixAndShiba.jpg";
 import main from "./main.webp";
+import logoGR from "./logoGR.png";
+import logoGR2 from "./logoGR2.png";
 
-export { alix, logo, fbk, insta, whatsApp, alixAndShiba, main };
+export { alix, logo, fbk, insta, whatsApp, alixAndShiba, main, logoGR, logoGR2 };

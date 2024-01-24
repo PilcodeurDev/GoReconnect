539442f64374c58ca9697080b39c73ad265ae52d
simon despres <simondprs62790@hotmail.fr>
2024-01-24 17:40:25 +0100

    footer RESPONSIVE 😎

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -11,7 +11,7 @@ export default function Footer() {
       href="https://my-visual-impact-prod-a80de0f47f5b.herokuapp.com/"
       target="_blank"
       rel="noreferrer"
-      className="lien"
+      className="link"
     >
       PilcodeurDev
     </a>
@@ -19,75 +19,65 @@ export default function Footer() {
 
   return (
     <footer>
-      <Link to="/#moveUp">
-        <img
-          src={logo}
-          alt="logo goreconnect"
-          className="logoKa25"
-        />
-      </Link>
-      <div className="textCenter">
-        <div>
-          <ul className="lienNavigation">
-            <li>
-              <Link to="/#moveUp" className="lien">
-                Accueil
-              </Link>
-            </li>
-            <li>
-              <Link to="/#laKinesiologie" className="lien">
-                Mes prestations
-              </Link>
-            </li>
-            <li>
-              <Link to="/#quiJeSuis" className="lien">
-                Qui suis-je ?
-              </Link>
-            </li>
-            <li>
-              <Link to="/#contact" className="lien">
-                Contact
-              </Link>
-            </li>
-            <li>
-              <Link to="/#reservation" className="lien">
-                Rendez-vous
-              </Link>
-            </li>
-          </ul>
+      <div className="links">
+        <div className="offset">
+          <Link to="/#moveUp">
+            <img
+              src={logo}
+              alt="logo goreconnect"
+              className="logo-goreconnect"
+            />
+          </Link>
         </div>
-        <div className="lienCenter">
-          <div>
-            <span>© 2024 Kinésiologie Animalière 25 Tous droits réservés.</span>
-            <span>
-              <Link to="/mention-legal" className="lien">
-                Mentions légales
-              </Link>
-            </span>
-            <span>
-              <Link to="/condition-general-de-vente" className="lien">
-                CGV
-              </Link>
-            </span>
-            <span>
-              <Link to="/condition-general-dutilisation" className="lien">
-                CGU
-              </Link>
-            </span>
-          </div>
-          <div>Design & Développement par {PilcodeurDev}</div>
+        <div className="interne-link-buttons">
+          <Link to="/#moveUp" className="link">
+            Accueil
+          </Link>
+          <Link to="/#laKinesiologie" className="link">
+            Mes prestations
+          </Link>
+          <Link to="/#quiJeSuis" className="link">
+            Qui suis-je ?
+          </Link>
+          <Link to="/#contact" className="link">
+            Contact
+          </Link>
+          <Link to="/#reservation" className="link">
+            Rendez-vous
+          </Link>
+        </div>
+        <div className="social-logos">
+          <a href="https://www.facebook.com/kinesiologieanimaliere25">
+            <img src={fbk} alt="logo facebook" />
+          </a>
+          <a href="https://www.instagram.com/kinesiologie_animaliere_25/">
+            <img src={insta} alt="logo instagram" />
+          </a>
+          <a href="https://www.instagram.com/kinesiologie_animaliere_25/">
+            <img src={whatsApp} alt="logo instagram" />
+          </a>
         </div>
       </div>
-      <div className="logos">
-        <a href="https://www.facebook.com/kinesiologieanimaliere25">
-          <img src={fbk} alt="logo facebook" />
-        </a>
-        <a href="https://www.instagram.com/kinesiologie_animaliere_25/">
-          <img src={insta} alt="logo instagram" />
-        </a>
-        <a href="https://www.instagram.com/kinesiologie_animaliere_25/">
-          <img src={whatsApp} alt="logo instagram" />
-        </a>
+      <div className="central-links">
+        <div>
+          <span>© 2024 Kinésiologie Animalière 25 Tous droits réservés.</span>
+          <span>
+            <Link to="/mention-legal" className="link">
+              Mentions légales
+            </Link>
+          </span>
+          <span>
+            <Link to="/condition-general-de-vente" className="link">
+              CGV
+            </Link>
+          </span>
+          <span>
+            <Link to="/condition-general-dutilisation" className="link">
+              CGU
+            </Link>
+          </span>
+        </div>
+        <div>Design & Développement par {PilcodeurDev}</div>
       </div>
     </footer>
   );
